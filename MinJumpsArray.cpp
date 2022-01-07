70756884b69d4882f75ee50a019f07641a51914f
aliprf <aliprf@gmail.com>
2022-01-06 20:11:28 -0700

    MinJumpsArray

---
+++ MinJumpsArray.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <map>
+
+using namespace std;
+
+int jump(vector<int> &A);
+int minJump(vector<int> &A, int current, vector<int> &jumpHistory);
+
+int main()
+{
+    vector<int> A = {2, 4, 1, 2,2,1, 1}; // {2,3,1,1,4,0}; //{2, 1, 1};//
+    cout << jump(A) << "\n\r";
+}
+
+int jump(vector<int> &A)
+{
+    if(A.empty()) return -1;
+    if(A.size()==1) return 0;
+
+    vector<int> jumpHistory(A.size());
+    for(int i=0; i < jumpHistory.size(); i++) jumpHistory[i]=-2;
+    int mJump =minJump(A, 0, jumpHistory);
+    return mJump;
+}
+
+int minJump(vector<int> &A, int current, vector<int> &jumpHistory)
+{
+    if(A[current]==0)
+    {
+        jumpHistory[current] = -1;
+        return -1;
+    }
+    if(current==A.size()) 
+    {
+        jumpHistory[current] = 0;
+        return 0;
+    }
+
+
+    if(A[current]+current >= A.size()-1)
+    {
+        jumpHistory[current] = 1;
+        return 1;
+    }
+    if(jumpHistory[current] != -2) return jumpHistory[current];
+    
+    int mJump = 1+minJump(A, current+1, jumpHistory);
+
+    for(int i=2; i<= A[current]; i++)
+    {
+        int jump = 1 + minJump(A, current+i, jumpHistory);
+        
+        if (jump<mJump)
+            mJump = jump;
+        
+    }
+    jumpHistory[current] = mJump;
+    return mJump;
+}
--- PalindromePartitioning.cpp
+++ PalindromePartitioning.cpp
@@ -9,10 +9,15 @@ int minCut(string A);
 vector<vector<int>> createIndices(string str);
 vector<char> findDistinctChars(string str);
 int findMaxPalindromic(vector<vector<int>> treeIndices);
+void createAllsubStrings(string str, vector<string> &subStrings);
+
 int main()
 {
-   cout << minCut("aabcdba") << "\n\r";
+//    cout << minCut("aabcdba") << "\n\r";
 //    cout << numDistinct("abbccaa", "bcaa") << "\n\r";
+    vector<string> subStrings;
+    createAllsubStrings("abc", subStrings);
+    cout << "\n\r";
 }
 
 int minCut(string A)
@@ -23,13 +28,14 @@ int minCut(string A)
     return A.size()-maxPalindStr;
 }
 
-
 int findMaxPalindromic(vector<vector<int>> treeIndices)
 {
     //for each char, we have found the indices which make the tree palindromic 
     // now we want to merge these chars:
+    //sort them by size of the furthest and see if you can merge them with smaller one.
     
 }
+
 vector<vector<int>> createIndices(string str)
 {
     vector<vector<int>> treeIndices;
@@ -64,4 +70,24 @@ vector<char> findDistinctChars(string str)
             
     }
     return seen;
+}
+
+void createAllsubStrings(string str, vector<string> &subStrings)
+{
+    if(str.length() == 1)
+    {
+        subStrings.push_back(str);
+        return;
+    }
+    
+    string prefixStr;
+    prefixStr.push_back(str[0]);
+    str.erase(0,1);
+    createAllsubStrings(str, subStrings);
+
+    int strL = subStrings.size();
+    for(int i=0; i< strL; i++)
+        subStrings.push_back(prefixStr+subStrings[i]);
+    subStrings.push_back(prefixStr);
+
 }
